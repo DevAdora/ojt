98ac60dd620c71cbebb7beae527b57f487792580
Rai <2r.reiyhes@gmail.com>
2025-02-18 08:38:40 +0800

    modified file path

---
+++ test.py
@@ -0,0 +1,33 @@
+#num1 = int(input())
+
+#numbers = list(map(int, input().split()))
+
+#result =  numbers[0] - numbers[1]
+
+#print(f" {numbers[0]} - {numbers[1]} = {result}")
+
+
+
+#list1 = list(map(int, input().split()))
+#list2 = list(map(int, input().split()))
+
+#if any(a == b for a, b in zip(list1, list2)):
+ #   print("no")
+ # #else:
+# print("yes")
+
+
+
+
+#def pyramid(rows=8):
+ #   for i in range(rows):
+ #       print (' '*(rows-i-1) + '*'*(2*i+1))
+
+#pyramid(8)
+  
+#pyramid(12)
+ 
+
+
+
+
